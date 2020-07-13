c3b44f0087581f7fe044db927eec10bc0f48a73e
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-13 03:10:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,11 +2,16 @@
 class Dog 
   def name=(dog_name)
     @this_dogs_name=dog_name
-  end 
   
     def name
     @this_dogs_name
-  end 
+  
+   def bark 
+      puts "Woof!"
+    
+    end #dog_name
+    end #name
+    end #woof
 end 
   
   
